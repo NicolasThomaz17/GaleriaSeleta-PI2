fcaa75a195840fae86952a0d8c104644dc3101a1
joaocavanhas <joaopedro.cavanhascolombo@gmail.com>
2026-05-13 15:34:24 -0300

    ADD: finalizei a tela esqueci-senha

--- galeria-seleta/src/app/app.routes.ts
+++ galeria-seleta/src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { LoginComponent } from './login/login.component';
+import { EsqueciSenhaComponent } from './esqueci-senha/esqueci-senha.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 export const routes: Routes = [
-  { path: '',         component: HomeComponent     },
-  { path: 'cadastro', component: CadastroComponent },
-  { path: 'login',    component: LoginComponent    },
-  { path: '**',       redirectTo: ''               }
+  { path: '',              component: HomeComponent         },
+  { path: 'cadastro',      component: CadastroComponent     },
+  { path: 'login',         component: LoginComponent        },
+  { path: 'esqueci-senha', component: EsqueciSenhaComponent },
+  { path: 'sobre', component: SobreComponent },
+  { path: '**',            redirectTo: ''                   }
 ];
